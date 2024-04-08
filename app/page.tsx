3fa7c68db9cc55f9ae175422fd9f02b12acac459
supreme2580 <victoromorogbe69@gmail.com>
2024-04-09 00:57:49 +0100

    action

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import battle from "./assets/battle.png";
 import blade from "./assets/blade.png";
 import trophy from "./assets/trophy.png";
 import winner from "./assets/winner.png";
+import starknetLogo from "./assets/strk.png"
 import WinnerCard from "./components/winner-card";
 import Countdown from "./components/countdown";
 
@@ -27,15 +28,32 @@ export default function Home() {
             </TabsList>
             <TabsContent value="arena" className="w-full h-full mt-10">
               <div className="w-full flex space-x-10 h-full">
-                <div className="w-3/4 h-[450px] bg-black bg-[url('./assets/players-bg.png')] bg-no-repeat bg-cover bg-center border-y-4 border-[#031C3A] rounded-2xl
+                <div className="w-3/4 h-[450px] space-y-10">
+                  <div className="w-full h-full bg-black bg-[url('./assets/players-bg.png')] bg-no-repeat bg-cover bg-center border-y-4 border-[#031C3A] rounded-2xl
                 flex flex-col items-center justify-center">
-                  <div className="w-[65%] flex justify-between items-center -mt-6 relative">
-                    <div className="absolute -right-[150px] -top-14">
-                      <Countdown seconds={7200} />
+                    <div className="w-[65%] flex justify-between items-center -mt-6 relative">
+                      <div className="absolute -right-[150px] -top-14">
+                        <Countdown seconds={7200} />
+                      </div>
+                      <Player image={avatar_1} name="GrowleR" power="110.6" />
+                      <Image src={battle} width={175} height={175} alt="battle" />
+                      <Player image={avatar_2} name="Mecurolia" power="50.3" />
                     </div>
-                    <Player image={avatar_1} name="GrowleR" power="110.6" />
-                    <Image src={battle} width={175} height={175} alt="battle" />
-                    <Player image={avatar_2} name="Mecurolia" power="50.3" />
+                  </div>
+                  <div className="w-full p-8 border-4 border-[#031C3A] rounded-xl bg-[#0E1824] flex justify-between items-center">
+                    <div className="space-y-1.5">
+                      <p className="text-lg text-[#D9DCDD]">Power</p>
+                      <div className="w-80 flex items-center border-2 border-[#031C3A] p-2.5 rounded-md">
+                        <input className="w-full border-0 outline-0 bg-transparent text-white mr-1.5" type="number" />
+                        <div className="bg-transparent border-2 border-[#031C3A] rounded-md p-1.5 w-fit">
+                          <Image src={starknetLogo} width={25} height={25} alt="strk" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="space-x-3.5 mt-10">
+                      <button className="bg-white px-20 py-3.5 rounded-2xl text-black">Flee</button>
+                      <button className="bg-gradient-to-r from-[#F15A24] via-[#F15A24]/90 to-[#9E005D] px-20 py-3.5 rounded-2xl text-white">Fight</button>
+                    </div>
                   </div>
                 </div>
                 <div className="w-[35%] h-fit border-4 border-[#031C3A] rounded-xl bg-[#0A1017] p-2.5">
