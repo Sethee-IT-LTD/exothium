11abb10a8ee4699a5df4b33cbd6bd0d036e8de7b
supreme2580 <victoromorogbe69@gmail.com>
2024-04-09 02:24:13 +0100

    active

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,8 @@ import winner from "./assets/winner.png";
 import starknetLogo from "./assets/strk.png"
 import WinnerCard from "./components/winner-card";
 import Countdown from "./components/countdown";
+import { Table, TableHead, TableHeader, TableRow } from "./components/table";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
 
@@ -20,7 +22,7 @@ export default function Home() {
     <main className="w-full min-h-screen bg-[url('./assets/spider-bg.png')] bg-no-repeat bg-cover bg-center bg-[#050C14]/95 bg-blend-darken">
       <Header />
       <div className="flex justify-center w-full">
-        <div className="flex flex-col items-center justify-center mt-5 px-16 h-full w-full max-w-[1560px]">
+        <div className="flex flex-col items-center justify-center mt-5 px-16 h-full w-full max-w-[1560px] pb-8">
           <Tabs defaultValue="arena" className="w-full flex flex-col items-center justify-center h-full">
             <TabsList className="bg-[#031C3A]">
               <TabsTrigger value="arena" className="data-[state=active]:bg-[#0A1017] data-[state=active]:text-white px-12 text-lg">Arena</TabsTrigger>
@@ -28,8 +30,8 @@ export default function Home() {
             </TabsList>
             <TabsContent value="arena" className="w-full h-full mt-10">
               <div className="w-full flex space-x-10 h-full">
-                <div className="w-3/4 h-[450px] space-y-10">
-                  <div className="w-full h-full bg-black bg-[url('./assets/players-bg.png')] bg-no-repeat bg-cover bg-center border-y-4 border-[#031C3A] rounded-2xl
+                <div className="w-3/4 h-full space-y-10">
+                  <div className="w-full h-[450px] bg-black bg-[url('./assets/players-bg.png')] bg-no-repeat bg-cover bg-center border-y-4 border-[#031C3A] rounded-2xl
                 flex flex-col items-center justify-center">
                     <div className="w-[65%] flex justify-between items-center -mt-6 relative">
                       <div className="absolute -right-[150px] -top-14">
@@ -51,9 +53,53 @@ export default function Home() {
                       </div>
                     </div>
                     <div className="space-x-3.5 mt-10">
-                      <button className="bg-white px-20 py-3.5 rounded-2xl text-black">Flee</button>
-                      <button className="bg-gradient-to-r from-[#F15A24] via-[#F15A24]/90 to-[#9E005D] px-20 py-3.5 rounded-2xl text-white">Fight</button>
+                      <button className="bg-white px-20 py-3.5 rounded-lg text-black">Flee</button>
+                      <button className="bg-gradient-to-r from-[#F15A24] via-[#F15A24]/90 to-[#9E005D] px-20 py-3.5 rounded-lg text-white">Fight</button>
                     </div>
+                  </div>
+                  {/* Table */}
+                  <div className="w-full">
+                    <div className="bg-[#031C3A] w-full px-5 py-2.5 rounded-t-xl flex space-x-0.5 items-center border-b-[#334155] border-b-2">
+                      <h4 className="text-lg text-[#CAD4E0] font-normal">Active Players</h4>
+                      <QuestionMarkCircleIcon className="w-5 h-5 text-[#CAD4E0]" />
+                    </div>
+                    <Table className="w-full px-5 border-r-2 border-transparent">
+                      <TableHeader className="w-full">
+                        <TableRow className="bg-[#031C3A] hover:bg-[#031C3A] border-b-[#334155] !border-b-2">
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-2.5">Gladiether</TableHead>
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-2.5">Username</TableHead>
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-2.5">Chances</TableHead>
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-2.5">Power</TableHead>
+                        </TableRow>
+                        <TableRow className="bg-gradient-to-r from-[#C1272D]/90 via-[#C1272D]/50 to-transparent border-b-[#334155] !border-b-2 
+                        border-l-3.5 border-l-[#C1272D] !border-r-0 w-full">
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-3.5">
+                            <Image src={avatar_1} width={65} height={65} alt="gladiether" />
+                          </TableHead>
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-3.5">GrowleR</TableHead>
+                          <TableHead className="text-lg font-normal py-3.5 text-[#F15A24]">99.96%</TableHead>
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-3.5">110.6</TableHead>
+                        </TableRow>
+                        <TableRow className="bg-gradient-to-r from-[#006837]/90 via-[#006837]/50 to-transparent border-b-[#334155] !border-b-2 
+                        border-l-3.5 border-l-[#006837] !border-r-0 w-full">
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-3.5">
+                            <Image src={avatar_1} width={65} height={65} alt="gladiether" />
+                          </TableHead>
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-3.5">GrowleR</TableHead>
+                          <TableHead className="text-lg text-[#F15A24] font-normal py-3.5">99.96%</TableHead>
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-3.5">110.6</TableHead>
+                        </TableRow>
+                        <TableRow className="bg-gradient-to-r from-[#8A5CF5]/90 via-[#8A5CF5]/50 to-transparent !border-b-0
+                        border-l-3.5 border-l-[#8A5CF5] !border-r-0 w-full">
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-3.5">
+                            <Image src={avatar_1} width={65} height={65} alt="gladiether" />
+                          </TableHead>
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-3.5">GrowleR</TableHead>
+                          <TableHead className="text-lg text-[#F15A24] font-normal py-3.5">99.96%</TableHead>
+                          <TableHead className="text-lg text-[#CAD4E0] font-normal py-3.5">110.6</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                    </Table>
                   </div>
                 </div>
                 <div className="w-[35%] h-fit border-4 border-[#031C3A] rounded-xl bg-[#0A1017] p-2.5">
