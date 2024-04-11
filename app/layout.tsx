f67b21fd706d57ab0a69738f7bed290d479e6b80
supreme2580 <victoromorogbe69@gmail.com>
2024-04-11 20:00:56 +0100

    added-music-to-background

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,26 +4,26 @@ import { useEffect, useState } from 'react'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-import useSound from "use-sound";
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   const [mounted, setMounted] = useState(false)
 
-  const [play] = useSound("/music/audio_1.mp3");
+  const audio = new Audio("/music/audio_1.mp3");
+
+  audio.addEventListener('ended', () => {
+    audio.play();
+  });
 
   useEffect(() => {
     setMounted(true)
   }, [mounted]);
 
-  play();
-
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} onClick={() => audio.play()}>
         {mounted && children}
       </body>
-    </html>
+    </html >
   )
 }
