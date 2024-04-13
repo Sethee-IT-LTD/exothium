09b788dc42b3a529a20751a23a5c41c575b203cb
supreme2580 <victoromorogbe69@gmail.com>
2024-04-13 16:56:41 +0100

    build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,21 +7,22 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
-  const [mounted, setMounted] = useState(false)
-
-  const audio = new Audio("/music/audio_1.mp3");
-
-  audio.addEventListener('ended', () => {
-    audio.play();
-  });
+  const [mounted, setMounted] = useState(false);
+  const [playing, setPlaying] = useState(false);
 
   useEffect(() => {
-    setMounted(true)
-  }, [mounted]);
+    setMounted(true);
+  }, []);
 
   return (
     <html lang="en">
-      <body className={inter.className} onClick={() => audio.play()}>
+      <body className={inter.className} onClick={() => {
+        if (typeof Audio !== "undefined" && playing == false) {
+          const audio = new Audio("/music/audio_1.mp3");
+          audio.play();
+          setPlaying(true);
+        }
+      }}>
         {mounted && children}
       </body>
     </html >
